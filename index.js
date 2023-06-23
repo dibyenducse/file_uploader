ac77f39ce90409f12cf24cb93f2d2d2b2d196598
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-24 00:24:45 +0600

    file upload filter applied

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const UPLOADS_FOLDER = './uploads/'
 
 //prepare the final multer upload object
 var upload = multer({
-    dest: UPLOADS_FOLDER
+    dest: UPLOADS_FOLDER,
+    limits: {
+        fileSize: 10000000,
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === "image/jpeg"
+        ) {
+            cb(null, true)
+        } else {
+            cb(new Error("Only .jpeg and .pdf file format allowed!"))
+        }
+    }
 })
 
 const app = express();
@@ -19,6 +30,17 @@ app.post('/', upload.fields([
     { name: "gallery", maxcount: 2 },
 ]), (req, res) => {
     res.send('hello world')
+})
+
+app.use((err, req, res, next) => {
+    if (err) {
+        if (err instanceof multer.MulterError) {
+            res.status(500).send("there was an upload error")
+        } else{res.status(500).send(err.message)}
+        
+    } else {
+        res.send("success")
+    }
 })
 
 app.listen(3000, () => {
