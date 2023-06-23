9cf86bec4517a9bf88a1a05f1bf7925d2c117cd8
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-24 03:15:09 +0600

    File Uploader done

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 const express = require('express')
 const multer = require('multer')
+const path = require("path")
 
 
 //file upload folder
 
 const UPLOADS_FOLDER = './uploads/'
 
+
+//define the storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADS_FOLDER)
+    },
+    filename: (req, file, cb) => {
+        const fileExt = path.extname(file.originalname)
+        const fileName = file.originalname
+            .replace(fileExt, "")
+            .toLowerCase().split(" ")
+            .join("-") + "-" + Date.now()
+        
+        cb(null, fileName + fileExt)
+    },
+})
+
 //prepare the final multer upload object
 var upload = multer({
-    dest: UPLOADS_FOLDER,
+    storage: storage,
     limits: {
         fileSize: 10000000,
     },
     fileFilter: (req, file, cb) => {
-        if (file.mimetype === "image/jpeg"
+        if (file.fieldname === "avatar") {
+            if (file.mimetype === "image/jpeg"
         ) {
             cb(null, true)
         } else {
-            cb(new Error("Only .jpeg and .pdf file format allowed!"))
+            cb(new Error("Only .jpeg file format allowed!"))
+            } 
+        } else if (file.fieldname === "gallary") {
+            if (file.mimetype === "application/pdf") {
+            cb(null, true)
+        } else {
+            cb(new Error("Only .pdf file format allowed!"))
+            } 
+        } else {
+            cb(new Error("There was an unknown error"))
         }
+        
     }
 })
 
@@ -27,8 +56,9 @@ const app = express();
 
 app.post('/', upload.fields([
     { name: "avatar", maxcount: 3 },
-    { name: "gallery", maxcount: 2 },
+    { name: "gallary", maxcount: 2 },
 ]), (req, res) => {
+    console.log(req.files)
     res.send('hello world')
 })
 
